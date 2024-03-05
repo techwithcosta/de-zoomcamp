18317ff492fb9dab7c61294ea17cc535918c2768
Diogo Costa <techwithcosta@gmail.com>
2024-03-05 10:57:47 +0000

    added spark local cluster

---
+++ 05-batch/06_spark_sql_local_cluster.py
@@ -0,0 +1,130 @@
+# python 06_spark_sql_local_cluster.py \
+#     --input_green=data/pq/green/2020/*/ \
+#     --input_yellow=data/pq/yellow/2020/*/ \
+#     --output=data/report/report-2020
+
+# URL="spark://desktop:7077"
+# spark-submit \
+#     --master="${URL}" \
+#     06_spark_sql_local_cluster.py \
+#         --input_green=data/pq/green/2021/*/ \
+#         --input_yellow=data/pq/yellow/2021/*/ \
+#         --output=data/report/report-2021
+
+# run local spark cluster
+# !cd /home/costa/spark/spark-3.5.0-bin-hadoop3/sbin
+# !./start-master.sh
+# !./start-worker.sh spark://desktop:7077
+# after finishing
+# !./stop-master.sh
+
+# http://localhost:8080/jobs/
+
+import argparse
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+print(pyspark.__version__)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input_green', required=True)
+parser.add_argument('--input_yellow', required=True)
+parser.add_argument('--output', required=True)
+
+args = parser.parse_args()
+
+input_green = args.input_green
+input_yellow = args.input_yellow
+output = args.output
+
+# removed "master"
+spark = SparkSession.builder \
+    .appName('test') \
+    .getOrCreate()
+
+df_green = spark.read.parquet(input_green)
+df_yellow = spark.read.parquet(input_yellow)
+
+df_green = df_green \
+    .withColumnRenamed('lpep_pickup_datetime', 'pickup_datetime') \
+    .withColumnRenamed('lpep_dropoff_datetime', 'dropoff_datetime')
+df_yellow = df_yellow \
+    .withColumnRenamed('tpep_pickup_datetime', 'pickup_datetime') \
+    .withColumnRenamed('tpep_dropoff_datetime', 'dropoff_datetime')
+
+common_columns = ['VendorID',
+                  'pickup_datetime',
+                  'dropoff_datetime',
+                  'store_and_fwd_flag',
+                  'RatecodeID',
+                  'PULocationID',
+                  'DOLocationID',
+                  'passenger_count',
+                  'trip_distance',
+                  'fare_amount',
+                  'extra',
+                  'mta_tax',
+                  'tip_amount',
+                  'tolls_amount',
+                  'improvement_surcharge',
+                  'total_amount',
+                  'payment_type',
+                  'congestion_surcharge'
+]
+
+df_green_sel = df_green \
+    .select(common_columns) \
+    .withColumn('service_type', F.lit('green'))
+
+df_yellow_sel = df_yellow \
+    .select(common_columns) \
+    .withColumn('service_type', F.lit('yellow'))
+
+df_trips_data = df_green_sel.unionAll(df_yellow_sel)
+
+df_trips_data.groupBy('service_type').count().show()
+
+df_trips_data.createOrReplaceTempView("trips_data")
+
+spark.sql("""
+SELECT * FROM trips_data LIMIT 10
+""").show()
+
+spark.sql("""
+SELECT
+    service_type,
+    count(1) as count
+FROM
+    trips_data
+GROUP BY
+    service_type
+""").show()
+
+df_result = spark.sql("""
+SELECT
+    -- Revenue grouping
+    PULocationID AS revenue_zone,
+    date_trunc('month', pickup_datetime) AS revenue_month,
+    service_type,
+
+    -- Revenue calculation
+    SUM(fare_amount) AS revenue_monthly_fare,
+    SUM(extra) AS revenue_monthly_extra,
+    SUM(mta_tax) AS revenue_monthly_mta_tax,
+    SUM(tip_amount) AS revenue_monthly_tip_amount,
+    SUM(tolls_amount) AS revenue_monthly_tolls_amount,
+    SUM(improvement_surcharge) AS revenue_monthly_improvement_surcharge,
+    SUM(total_amount) AS revenue_monthly_total_amount,
+    SUM(congestion_surcharge) AS revenue_monthly_congestion_surcharge,
+
+    -- Additional calculations
+    AVG(passenger_count) AS avg_monthly_passenger_count,
+    AVG(trip_distance) AS avg_monthly_trip_distance
+FROM
+    trips_data
+GROUP BY
+    1, 2, 3
+""")
+
+df_result.write.parquet(output, mode='overwrite')
